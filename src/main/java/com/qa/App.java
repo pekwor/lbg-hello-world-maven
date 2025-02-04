ceee86d705ae167e46c4200fd0f49e4db1d8d171
Pekwor <pekwor@.qa.com>
2025-02-04 14:18:46 +0000

    committing change

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,9 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
-        System.out.println(sayHelloToSomeone("Alice"));
-        System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
+        System.out.println(sayHelloToSomeone("Pekwor"));
+        System.out.println(sayHelloToSomeone("Bev"));
+        System.out.println(sayHelloToSomeone("Nitish"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
